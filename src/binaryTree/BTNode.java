7a3611db290e7ae14da87382de1a18f45a6de141
Vanja <vujovic.vanjanja@gmail.com>
2024-02-05 12:34:35 +0100

    Hafman

--- src/binaryTree/BTNode.java
+++ src/binaryTree/BTNode.java
@@ -1,37 +1,50 @@
 package binaryTree;
 
-public class BTNode<T extends Comparable<T>> {
+public class BTNode<T extends Comparable<T>> implements Comparable<BTNode<T>>{
 	private T info;
 	private BTNode left;
 	private BTNode right;
+
 	public BTNode(T info, BTNode left, BTNode right) {
 		this.info = info;
 		this.left = left;
 		this.right = right;
 	}
+
 	public BTNode(T info) {
 		this(info, null, null);
 	}
+
 	public T getInfo() {
 		return info;
 	}
+
 	public void setInfo(T info) {
 		this.info = info;
 	}
+
 	public BTNode getLeft() {
 		return left;
 	}
+
 	public void setLeft(BTNode left) {
 		this.left = left;
 	}
+
 	public BTNode getRight() {
 		return right;
 	}
+
 	public void setRight(BTNode right) {
 		this.right = right;
 	}
-	
+
 	public String toString() {
 		return info.toString();
+	}
+
+	@Override
+	public int compareTo(BTNode<T> o) {
+		return info.compareTo(o.info);
 	}
 }
---
+++ src/hafman/Hafman.java
@@ -0,0 +1,98 @@
+package hafman;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+import binaryTree.BTClassic;
+import binaryTree.BTNode;
+
+public class Hafman {
+	private static class Node implements Comparable<Node> {
+		int frequency;
+		char c;
+
+		public Node(int frequency, char c) {
+			this.frequency = frequency;
+			this.c = c;
+		}
+
+		public int compareTo(Node o) {
+			return frequency - o.frequency;
+		}
+
+		public String toString() {
+			return "{" + c + ", " + frequency + "}";
+		}
+	}
+
+	private LinkedList<Node> l;
+	private BTClassic<Node> bin;
+
+	public Hafman(String text) {
+		frequencies(text);
+		construct();
+	}
+
+	private void frequencies(String text) {
+		l = new LinkedList<Node>();
+		HashMap<Character, Node> map = new HashMap<Character, Node>();
+		for (int i = 0; i < text.length(); i++) {
+			char cur = text.charAt(i);
+			Node tren = map.get(cur);
+			if (tren != null) {
+				tren.frequency++;
+				continue;
+			}
+			tren = new Node(1, cur);
+			l.addLast(tren);
+			map.put(cur, tren);
+		}
+	}
+
+	private void construct() {
+		PriorityQueue<BTNode<Node>> prio = new PriorityQueue<BTNode<Node>>();
+		Iterator<Node> it = l.iterator();
+		while(it.hasNext()) {
+			Node cur = it.next();
+			BTNode<Node> n = new BTNode<Node>(cur);
+			prio.add(n);
+		}
+		while(prio.size() >= 2) {
+			BTNode<Node> n1 = prio.poll();
+			BTNode<Node> n2 = prio.poll();
+			Node cur = new Node(n1.getInfo().frequency + n2.getInfo().frequency, '?');
+			BTNode<Node> n = new BTNode<Node>(cur, n1, n2);
+			prio.add(n);
+		}
+		bin = new BTClassic(prio.poll());
+	}
+	
+	public void print() {
+		BTNode<Node> root = bin.getRoot();
+		if(root.getLeft() == null && root.getRight() == null) {
+			System.out.println(root.getInfo() + ": " + "1");
+			return;
+		}
+		print(root, new String(""));
+	}
+	
+	private void print(BTNode<Node> cur, String s) {
+		if(cur.getLeft() == null && cur.getRight() == null) {
+			System.out.println(cur.getInfo() + ": " + s);
+			return;
+		}
+		if(cur.getLeft() != null) {
+			print(cur.getLeft(), s + '1');
+		}
+		if(cur.getRight() != null) {
+			print(cur.getRight(), s + '0');
+		}
+	}
+	
+	public static void main(String args[]) {
+		Hafman h = new Hafman("ana voli milovana");
+		h.print();
+	}
+}
